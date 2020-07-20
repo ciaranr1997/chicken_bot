41b13c379785c4abcaeebe9e2c116f79a359f182
Ciaran Reddington <ciaranr1997@gmail.com>
2020-07-20 04:56:29 +0100

    Starting of page to manage your own quotes online.

--- website/routes/quotes.js
+++ website/routes/quotes.js
@@ -9,12 +9,23 @@ router.get('/',(req,res)=>{
 	fs.readFile('html/quotes.html', async (e, data) => {
 		let sql = require("../../sql.js");
 		sql.connect();
-		quoteList = await sql.syncQuery("select * from fowl_quotes where user_id='"+user.id+"'");
+		quoteList = await sql.syncQuery("select * from fowl_quotes where user_id='189514636318605313'");
 		sql.close();
+		output = "";
+		for(i=0;i<quoteList.length;i++)
+		{
+			output+="<li class=\"\">";
+			output+="<span class=\"quote\">";
+			output+=quoteList[i].quote;
+			output+="</span>";
+			output+="<button class=\"redactbtn\" id=\"redact-"+quoteList.id+"\">REDACT</button>";
+			output+="</li>"
+		}
 		console.log(quoteList);
 		html = data.toString();
 		html = html.replace("${site.header}",header);
 		html = html.replace("${site.nav}",nav);
+		html = html.replace("${quotes.list}",output);
 		html = html.replace("${user.image}",req.user.image);
 
 		res.writeHead(200, {'Content-Type': 'text/html'});
